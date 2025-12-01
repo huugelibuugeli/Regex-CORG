0fbe086db2d2e409e69ee05ac4df3489f37fe4b5
KuroPebbles <darkchoaskeeper@gmail.com>
2025-12-01 17:43:05 -0500

    Update final.asm

--- final.asm
+++ final.asm
@@ -46,3 +46,46 @@ dash:
 caret:
 	#code
 	b parse_loop
+
+print_match:
+    addi $sp, $sp, -16
+    sw   $ra, 12($sp)
+    sw   $t0, 8($sp)
+    sw   $t1, 4($sp)
+    sw   $t2, 0($sp)
+
+    # If not first match, print ", "
+    beq  $a3, $zero, pm_no_sep
+
+    la   $a0, comma_space
+    li   $v0, 4          # print_string
+    syscall
+
+pm_no_sep:
+    # t0 = base address of input string
+    move $t0, $a0
+    # t1 = current index (start)
+    move $t1, $a1
+    # t2 = remaining length
+    move $t2, $a2
+
+pm_loop:
+    beq  $t2, $zero, pm_done   # len == 0 -> done
+
+    add  $t3, $t0, $t1         # addr = base + index
+    lb   $a0, 0($t3)           # load char
+    li   $v0, 11               # print_char
+    syscall
+
+    addi $t1, $t1, 1           # index++
+    addi $t2, $t2, -1          # len--
+    j    pm_loop
+
+pm_done:
+    # restore
+    lw   $t2, 0($sp)
+    lw   $t1, 4($sp)
+    lw   $t0, 8($sp)
+    lw   $ra, 12($sp)
+    addi $sp, $sp, 16
+    jr   $ra
